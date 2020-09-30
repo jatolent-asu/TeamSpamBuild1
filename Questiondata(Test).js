e0f638b4a405ef30fafc5396d892a6fb63fb6183
todd <todd.despain@asu.edu>
2020-09-30 14:37:55 -0700

    Edited questions

--- Questiondata(Test).js
+++ Questiondata(Test).js
@@ -33,5 +33,61 @@ var jsonData = [
         "opt2" : "Reaper",
         "opt3" : "Header",
         "answer" : "Reaper"
-    }
+    },
+    {
+        "q": "What is a ‘Cyber risk’?",
+        "opt1": "A risk only constituting computers or information technology",
+        "opt2": "Any risk of financial loss, disruption or damage to the reputation of an organization from some sort of failure of its information technology systems",
+        "opt3": "A risk that comes from the internet to an entity.",
+        "answer": "opt2"
+      },
+      {
+        "q": "What is Fraud?",
+        "opt1": "Any means a person uses to gain an unfair advantage over another person using false statements, representations,or disclosures, as a material fact to a victim to act with the intent to deceive based on misrepresentation resulting in a injury or loss",
+        "opt2": "A federal crime in the United Sates of America",
+        "opt3": "A type of sausage",
+        "answer": "a"
+      },
+      {
+        "q": "What are red flags?",
+        "opt1": "Signs to stay away from someone or some event",
+        "opt2": "Flags that symbolize to stop or that there is trouble",
+        "opt3": "Potential symptoms of fraud",
+        "answer": "opt3"
+      },
+      {
+        "q": "What are the most common threat or attack vectors?",
+        "opt1": "Malware, Hacking, Phishing, Social Engineering",
+        "opt2": "Old People, Tech Illerate, Outdated Software, Bad Passwords",
+        "opt3": "Internet, Email, Scams, Sketchy Sites",
+        "answer": "a"
+      },
+      {
+        "q": "What is malware?",
+        "opt1": "Something that takes all your data",
+        "opt2": "An infection that runins a computer forever",
+        "opt3": "Software designed to harm or also to Hack or Phish",
+        "answer": "opt3"
+      },
+      {
+        "q": "What is phishing?",
+        "opt1": "A fraudulent attempt to obtain sensitive information by disguise using a trustworthy entity usually through EMAIL",
+        "opt2": "A slang word that means to be popular amongst the people",
+        "opt3": "Searching through the internet looking for anything relative a search",
+        "answer": "a"
+      },
+      {
+        "q": "What is social engineering?",
+        "opt1": "Psycology used to build better people",
+        "opt2": "Techniques or tricks on people to gain physical or logical access to confidential informational engineering",
+        "opt3": "Hacking the human brain",
+        "answer": "opt2"
+      },
+      {
+        "q": "What is computer fraud?",
+        "opt1": "Fraud done by a computer or bot",
+        "opt2": "Fraud accomplished using computers",
+        "opt3": "Fraud that requires computer technology to perpetrate at any stage of the data processing cycle.",
+        "answer": "opt3"
+      }
 ];
