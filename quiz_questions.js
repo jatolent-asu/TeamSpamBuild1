494631f1db969fb33b7238524eed6666809e74aa
todd <todd.despain@asu.edu>
2020-10-01 16:03:27 -0700

    Updated Questions, Homepage, and Nav-Bar

--- quiz_questions.js
+++ quiz_questions.js
@@ -1,92 +1,114 @@
 var quiz_data = [
-    {
-      question: "What is a ‘Cyber risk’?",
-      answers: {
-        a: "Sample Answers",
-        b: "Any risk of financial loss, disruption or damage to the reputation of an organization from some sort of failure of its information technology systems",
-        c: "Sample Answers"
+        {
+      "question": "What is a ‘Cyber risk’?",
+      "answers": {
+        "a": "A risk only constituting computers or information technology",
+        "b": "Any risk of financial loss, disruption or damage to the reputation of an organization from some sort offailure of its information technology systems",
+        "c": "A risk that comes from the internet to an entity."
       },
-      correctAnswer: "b"
+      "correctAnswer": "b"
     },
     {
-      question: "What is Fraud?",
-      answers: {
-        a: "Fraud is defined as, any means a person uses to gain an unfair advantage over another person",
-        b: "Sample Answers",
-        c: "Sample Answers"
+      "question": "What is Fraud?",
+      "answers": {
+        "a": "Any means a person uses to gain an unfair advantage over another person using false statements, representations,or disclosures, as a material fact to a victim to act with the intent to deceive based on misrepresentation resulting in a injury or loss",
+        "b": "A federal crime in the United Sates of America",
+        "c": "A type of sausage"
       },
-      correctAnswer: "a"
+      "correctAnswer": "a"
     },
     {
-      question: "What are red flags?",
-      answers: {
-        a: "Sample Answers",
-        b: "Sample Answers",
-        c: "Red flags are considered potential symptoms of fraud"
+      "question": "What are red flags?",
+      "answers": {
+        "a": "Signs to stay away from someone or some event",
+        "b": "Flags that symbolize to stop or that there is trouble",
+        "c": "Potential symptoms of fraud"
       },
-      correctAnswer: "c"
+      "correctAnswer": "c"
     },
     {
-      question: "What are the most common threat or attack vectors?",
-      answers: {
-        a: "The most common Threat or Attack Vectors are: (1) Malware, (2) Hacking, (3) Phishing & (4) Social Engineering",
-        b: "Sample Answers",
-        c: "Sample Answers"
+      "question": "What are the most common threat or attack vectors?",
+      "answers": {
+        "a": "Malware, Hacking, Phishing, Social Engineering",
+        "b": "Old People, Tech Illerate, Outdated Software, Bad Passwords",
+        "c": "Internet, Email, Scams, Sketchy Sites"
       },
-      correctAnswer: "a"
+      "correctAnswer": "a"
     },
     {
-      question: "What is malware?",
-      answers: {
-        a: "Sample Answers",
-        b: "Sample Answers",
-        c: "Malware is software designed do harm or also to Hack or Phish"
+      "question": "What is malware?",
+      "answers": {
+        "a": "Something that takes all your data",
+        "b": "An infection that runins a computer forever",
+        "c": "Software designed to harm or also to Hack or Phish"
       },
-      correctAnswer: "c"
+      "correctAnswer": "c"
     },
     {
-      question: "What is phishing?n",
-      answers: {
-        a: "Phishing is the fraudulent attempt to obtain sensitive information by disguise using a trustworthy entity usually through EMAIL",
-        b: "Sample Answers",
-        c: "Sample Answers"
+      "question": "What is phishing?",
+      "answers": {
+        "a": "A fraudulent attempt to obtain sensitive information by disguise using a trustworthy entity usually through EMAIL",
+        "b": "A slang word that means to be popular amongst the people",
+        "c": "Searching through the internet looking for anything relative a search"
       },
-      correctAnswer: "a"
+      "correctAnswer": "a"
     },
     {
-      question: "What is social engineering?",
-      answers: {
-        a: "Sample Answers",
-        b: "Social Engineering uses techniques or tricks on people to gain physical or logical access to confidential informational engineering",
-        c: "Sample Answers"
+      "question": "What is social engineering?",
+      "answers": {
+        "a": "Psycology used to build better people",
+        "b": "Techniques or tricks on people to gain physical or logical access to confidential informational engineering",
+        "c": "Hacking the human brain"
       },
-      correctAnswer: "b"
+      "correctAnswer": "b"
     },
     {
-      question: "Sample Question",
-      answers: {
-        a: "Sample Answers",
-        b: "Sample Answers",
-        c: "Sample Answers"
+      "question": "What is computer fraud?",
+      "answers": {
+        "a": "Fraud done by a computer or bot",
+        "b": "Fraud accomplished using computers",
+        "c": "Fraud that requires computer technology to perpetrate at any stage of the data processing cycle."
       },
-      correctAnswer: "c"
+      "correctAnswer": "c"
     },
     {
-      question: "What is computer fraud?",
-      answers: {
-        a: "Sample Answers",
-        b: "Sample Answers",
-        c: "Computer fraud is any fraud that requires computer technology to perpetrate & can at any stage of the data processing cycle."
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Sample Question",
-      answers: {
-        a: "Sample Answers",
-        b: "Sample Answers",
-        c: "Sample Answers"
-      },
-      correctAnswer: "c"
-    }
+      "question" : "What are the most common threat or attack vectors?",
+      "answers": {
+      "a" : "Malware",
+      "b" : "Both a and b",
+      "c" : "Social Engineering"},
+      "correctAnswer" : "b"
+  },
+  {
+      "question" : "What is malware?",
+      "answers": {
+      "a" : "Software that can cause harm to the host computer",
+      "b" : "Unsolicited messages sent in bulk over electronic mailing system.",
+      "c" : "Software that bypasses the established authentication process to allow access to a system."},
+      "correctAnswer" : "a"
+  },
+  {
+      "question" : "What is the First Computer Worm ?",
+      "answers": {
+      "a" : "ILOVEYOU",
+      "b" : "Mellssa",
+      "c" : "Creeper"},
+      "correctAnswer" : "c"
+  },
+  {
+      "question" : "What is ILOVEYOU?",
+      "answers": {
+      "a" : "The Virus Infects More Then 50 Million Computers",
+      "b" : "The Virus Infects Mobile Device",
+      "c" : "The Antivirus That Help User Protact Their Computers"},
+      "correctAnswer" : "a"
+  },
+  {
+      "question" : "What is the First Cybersecurity Program",
+      "answers": {
+      "a" : "Chapper",
+      "b" : "Reaper",
+      "c" : "Header"},
+      "correctAnswer" : "b"
+  }
 ];
